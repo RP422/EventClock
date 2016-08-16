ce6f89cfe470e6c6d077b6e80243840f3d517b30
Mike Lang <mlang@student.neumont.edu>
2016-08-16 08:42:33 -0600

    Commit to include updates and changes made since last commit

--- ClockWithEvents/Clock.cs
+++ ClockWithEvents/Clock.cs
@@ -8,12 +8,7 @@ namespace ClockWithEvents
 {
     public class Clock
     {
-        //This line defines a type.  It is similar in purpose to "public class AnInternalClass{...}"
         public delegate void TimeChangedDelegate(int currentTime);
-
-        //This line declares a variable of a particular type; a custom delegate we created above.  
-        //It is similar to the "public ... anInternalClassInstance" line below
-        //Normally class level members should be private.  Events are public if we want external classes to handle them.
         public event TimeChangedDelegate MillisecondChanged;
         public event TimeChangedDelegate SecondChanged;
         public event TimeChangedDelegate MinuteChanged;
@@ -28,35 +23,24 @@ namespace ClockWithEvents
 
         public void Start()
         {
-            //will throw, because anInternalClassInstance is not set to an instance
-            //Console.WriteLine(anInternalClassInstance.MyProperty);
-
             for (int i = 0; i < 1000000; i++)
             {
                 milliseconds++;
-                //Sleeping for 1 millisecond is a little silly and inefficient, but it will help keep our math easy to follow
                 System.Threading.Thread.Sleep(1);
 
                 OnMillisecondChanged();
-
                 if (milliseconds % MILLISECONDS_IN_SECOND == 0)
                 {
                     OnSecondChanged();
                 }
-
                 if (milliseconds % (MILLISECONDS_IN_SECOND * SECONDS_IN_MINUTE) == 0)
                 {
                     OnMinuteChanged();
                 }
-
                 if (milliseconds % (MILLISECONDS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR) == 0)
                 {
                     OnHourChanged();
                 }
-
-                //The Clock is no longer aware of the console.  It notifies calling code of changes through events
-                //rather than writing or printing changes itself.
-                //Console.WriteLine(seconds);
             }            
         }
 
@@ -67,7 +51,6 @@ namespace ClockWithEvents
                 HourChanged.Invoke(milliseconds / (MILLISECONDS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR) % HOURS_IN_DAY);
             }
         }
-
         private void OnMinuteChanged()
         {
             if (MinuteChanged != null)
@@ -75,16 +58,13 @@ namespace ClockWithEvents
                 MinuteChanged.Invoke(milliseconds / (MILLISECONDS_IN_SECOND * SECONDS_IN_MINUTE) % MINUTES_IN_HOUR);
             }
         }
-
         private void OnSecondChanged()
         {
             if (SecondChanged != null)
             {
-                //this line is incorrect.  Fix it! :)
                 SecondChanged.Invoke(milliseconds / MILLISECONDS_IN_SECOND % SECONDS_IN_MINUTE);
             }
         }
-
         private void OnMillisecondChanged()
         {
             if (MillisecondChanged != null)
--- WPFClock/MainWindow.xaml.cs
+++ WPFClock/MainWindow.xaml.cs
@@ -1,123 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ClockWithEvents;
-using System.Windows.Threading;
 
 namespace WPFClock
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private Clock ticker = new Clock();
-
         private delegate void NoArg();
 
         public MainWindow()
         {
             InitializeComponent();
-
-            /*
-             * We could also use the Action type instead of NoArg and save some code.  
-             * But then we'd need to understand Generics...
-             * so I'll stick with NoArg for now.
-            */
             NoArg start = ticker.Start;
-
-            /*
-             *  .NET prevents us from updating UI elements from another thread.
-             *  Our clock uses Thread.Sleep which would make our app look like it crashed.
-             *  We'll use a separate thread for the clock.Start method, then use the Dispatcher
-             *  to update the UI in its own sweet time on its own sweet thread.  Think of 
-             *  queueing up a message that is then processed by the UI thread when it's able.
-             *  
-             *  Importantly, we don't have to change the Clock class to take advantage of threading.
-             *  All the Dispatch/BeginInvoke magic happens here in the client code.
-             * 
-             */
-            
-            /* 
-            ticker.MillisecondChanged += Ticker_MillisecondsChangedOnDifferentThread;
-            ticker.SecondChanged += Ticker_SecondsChangedOnDifferentThread;
-            ticker.MinuteChanged += Ticker_MinutesChangedOnDifferentThread;
-            ticker.HourChanged += Ticker_HoursChangedOnDifferentThread;
-            */
             start.BeginInvoke(null, null);
-            
         }
         
         private void Ticker_UpdateMilliseconds(int currentTime)
         {
-            /*
-             * This method is executed by the UI thread, and so can modify the label directly.
-             */
             MillisecondsDisplay.Content = currentTime.ToString("D3");
         }
         private void Ticker_UpdateSeconds(int currentTime)
         {
-            /*
-             * This method is executed by the UI thread, and so can modify the label directly.
-             */
             SecondsDisplay.Content = currentTime.ToString("D2");
         }
         private void Ticker_UpdateMinutes(int currentTime)
         {
-            /*
-             * This method is executed by the UI thread, and so can modify the label directly.
-             */
             MinutesDisplay.Content = currentTime.ToString("D2");
         }
         private void Ticker_UpdateHours(int currentTime)
         {
-            /*
-             * This method is executed by the UI thread, and so can modify the label directly.
-             */
             HoursDisplay.Content = currentTime.ToString("D2");
         }
         
         private void Ticker_MillisecondsChangedOnDifferentThread(int currentTime)
         {
-            /*
-             * Here's where the Clock's thread will put a message on the UI thread's queue of work,
-             * again, through the use of a delegate
-             */
             MillisecondsDisplay.Dispatcher.BeginInvoke(new Action<int>(Ticker_UpdateMilliseconds), currentTime);
         }
         private void Ticker_SecondsChangedOnDifferentThread(int currentTime)
         {
-            /*
-             * Here's where the Clock's thread will put a message on the UI thread's queue of work,
-             * again, through the use of a delegate
-             */
             SecondsDisplay.Dispatcher.BeginInvoke(new Action<int>(Ticker_UpdateSeconds), currentTime);
         }
         private void Ticker_MinutesChangedOnDifferentThread(int currentTime)
         {
-            /*
-             * Here's where the Clock's thread will put a message on the UI thread's queue of work,
-             * again, through the use of a delegate
-             */
             MinutesDisplay.Dispatcher.BeginInvoke(new Action<int>(Ticker_UpdateMinutes), currentTime);
         }
         private void Ticker_HoursChangedOnDifferentThread(int currentTime)
         {
-            /*
-             * Here's where the Clock's thread will put a message on the UI thread's queue of work,
-             * again, through the use of a delegate
-             */
             HoursDisplay.Dispatcher.BeginInvoke(new Action<int>(Ticker_UpdateHours), currentTime);
         }
 
@@ -125,7 +55,6 @@ namespace WPFClock
         {
             CheckBox box = sender as CheckBox;
             ManageHourWiring(box.IsChecked);
-            //HoursDisplay.Dispatcher.BeginInvoke(new Action<bool?>(ManageHourWiring), box.IsChecked);
         }
         private void ManageHourWiring(bool? flag)
         {
@@ -142,7 +71,6 @@ namespace WPFClock
         {
             CheckBox box = sender as CheckBox;
             ManageMinuteWiring(box.IsChecked);
-            //MinutesDisplay.Dispatcher.Invoke(new Action<bool?>(ManageMinuteWiring), box.IsChecked);
         }
         private void ManageMinuteWiring(bool? flag)
         {
@@ -160,7 +88,6 @@ namespace WPFClock
         {
             CheckBox box = sender as CheckBox;
             ManageSecondWiring(box.IsChecked);
-            //SecondsDisplay.Dispatcher.Invoke(new Action<bool?>(ManageSecondWiring), box.IsChecked);
         }
         private void ManageSecondWiring(bool? flag)
         {
@@ -178,7 +105,6 @@ namespace WPFClock
         {
             CheckBox box = sender as CheckBox;
             ManageMillisecondWiring(box.IsChecked);
-            //MillisecondsDisplay.Dispatcher.Invoke(new Action<bool?>(ManageMillisecondWiring), box.IsChecked);
         }
         private void ManageMillisecondWiring(bool? flag)
         {
